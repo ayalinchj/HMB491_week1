fd15d22db21631d54f91b1ed1550b553933387f2
ayalinch <ayalinchj@gmail.com>
2025-09-24 11:06:52 -0400

    Updated plot script and figure for graded_question branch

--- week1_plot.py
+++ week1_plot.py
@@ -2,12 +2,12 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 # TODO: load the dataset as pandas dataframe
+df_teeth = pd.read_csv("mammal_teeth.csv")
 
-plt.figure(figsize=(5, 10)) # set figure size
+plt.figure(figsize=(15, 15)) # set figure size
 plt.scatter(x=df_teeth['Top incisors'],
-y=df_teeth['MAMMAL']) # set figure x, y axis
-plt.gca().xaxis.set_visible(False)
-
+            y=df_teeth['MAMMAL']) # set figure x, y axis
+plt.gca().xaxis.set_visible(True)
 # TODO: change the title name to include your name
-plt.title("plot for mammal_teeth dataset")
+plt.title("Top Incisors Count Across Mammal Species – Ayalinch Jonathan")
 plt.savefig("mammal_teeth_scatterplot.png", dpi=150) # save the figure
